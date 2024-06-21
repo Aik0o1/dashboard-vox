1f54731ccad7bc733376d2b57c36d84e82cf3a93
Cibelle Maciel <108502128+cibellemc@users.noreply.github.com>
2024-06-21 11:20:33 -0300

    feat: separação de dependências

--- dashboard.py
+++ dashboard.py
@@ -1,19 +1,18 @@
 import streamlit as st
-import pandas as pd
-import plotly.express as px
-from graphs import mapHeat, pieMargin
-
+from components import layout
+from main import load_and_prepare_data
 
 st.set_page_config(layout='wide')
 st.title("Informações Empresariais")
 
-df = pd.read_csv("assets/dadosFakess.csv")
-df['abertura'] = pd.to_datetime(df['abertura'])
-df['fechamento'] = pd.to_datetime(df['fechamento'])
-df = df.sort_values(by='municipio')
-
 # Criar abas
 tab1, tab2 = st.tabs(["Aberturas e Fechamentos", "Atividade e Inatividade"])
+
+# Recebe dados tratados
+(df, total_aberturas, total_fechamentos, margem_abertura_fechamento, 
+ df_margem_abertura_fechamento, merge_abertura_fechamento,
+ total_ativas, total_inativas, margem_ativas_inativas, 
+ df_margem_ativas_inativas, merge_ativas_inativas) = load_and_prepare_data()
 
 # Sidebar
 ano = st.sidebar.selectbox("Ano", ["Todos"] + list(range(2000, 2025)))
@@ -31,72 +30,11 @@ if porte != "Todos":
 if atividade != "Todas":
     df = df[df["atividade"] == atividade]
 
-
-#Total de aberturas, fechamentos e margem
-total_aberturas = df['anoAbertura'].count()
-total_fechamentos = df['anoFechamento'].count()
-margem = total_aberturas - total_fechamentos
-
-# Preparar dados para o gráfico de margem
-data_margem = {
-    'Tipo': ['Aberturas', 'Fechamentos'],
-    'Quantidade': [total_aberturas, total_fechamentos]
-}
-
-df_margem = pd.DataFrame(data_margem)
-
-
-#filtro de dados para grafico de abertura vs fechamentos
-aberturas_por_ano = df.groupby('anoAbertura').size().reset_index(name='aberturas')
-fechamentos_por_ano = df.dropna(subset=['anoFechamento']).groupby('anoFechamento').size().reset_index(name='fechamentos')
-
-aberturas_por_ano.rename(columns={'anoAbertura': 'ano'}, inplace=True)
-fechamentos_por_ano.rename(columns={'anoFechamento': 'ano'}, inplace=True)
-
-dados_combined = pd.merge(aberturas_por_ano, fechamentos_por_ano, on='ano', how='outer').fillna(0)
-
-
-# graficos
-fig_abertura_fechamento = px.line(dados_combined, x='ano', y=['aberturas', 'fechamentos'], 
-    title='Número de Aberturas e Fechamentos por Ano',
-    labels={'value': 'Quantidade', 'variable': 'Tipo'})
-
 #construindo a pagina
 with tab1:
-    
-    # Separando espaços
-    bloco_total_aberturas, blocoTotalFechamentos, blocoMargem = st.columns(3)
-    bloco_grafico_abertura_fechamento = subtab1, subtab2 = st.tabs(["Gráfico", "Tabela"])
-    bloco_mapa, bloco_grafico_aberturas, bloco_grafico_fechamentos = st.columns(3)
-    blocoTabela = st.columns(1)
-    
-    with bloco_total_aberturas:
-        st.metric(label='Total de aberturas', value=total_aberturas)
-
-    with blocoTotalFechamentos:
-        st.metric(label='Total de Fechamentos', value=total_fechamentos)
-
-    with blocoMargem:
-        col1, col2 = st.columns(2)
-        with col1:
-            st.metric(label='Margem', value=margem)
-        with col2:
-            st.plotly_chart(pieMargin.graph(df_margem), use_container_width=True)
-
-
-    with bloco_grafico_abertura_fechamento[0]:
-        subtab1.plotly_chart(fig_abertura_fechamento)
-        
-    with bloco_grafico_abertura_fechamento[1]:
-        subtab2.write(dados_combined)
-
-
-    with bloco_mapa:
-        st.plotly_chart(mapHeat.plotMap(df))
-
-        
-                    
+    titulo_positivo, titulo_negativo = "aberturas", "fechamentos"
+    layout.layout(total_aberturas, total_fechamentos, margem_abertura_fechamento, df_margem_abertura_fechamento, merge_abertura_fechamento, titulo_positivo, titulo_negativo)
     
 with tab2:
-    
-    st.write("Loading...")
+    titulo_positivo, titulo_negativo = "ativas", "inativas"
+    layout.layout(total_ativas, total_inativas, margem_ativas_inativas, df_margem_ativas_inativas, merge_ativas_inativas, titulo_positivo, titulo_negativo)
---
+++ main.py
@@ -0,0 +1,58 @@
+import pandas as pd
+
+def load_and_prepare_data():
+    df = pd.read_csv("assets/dadosFakess.csv")
+    
+    df['abertura'] = pd.to_datetime(df['abertura'])
+    df['fechamento'] = pd.to_datetime(df['fechamento'])
+    df = df.sort_values(by='municipio')
+
+    #Total de aberturas, fechamentos e margem_abertura_fechamento
+    total_aberturas = df['anoAbertura'].count()
+    total_fechamentos = df['anoFechamento'].count()
+    margem_abertura_fechamento = total_aberturas - total_fechamentos
+
+    # Preparar dados para o gráfico de margem_abertura_fechamento
+    data_margem_abertura_fechamento = {
+        'Tipo': ['Aberturas', 'Fechamentos'],
+        'Quantidade': [total_aberturas, total_fechamentos]
+    }
+
+    df_margem_abertura_fechamento = pd.DataFrame(data_margem_abertura_fechamento)
+    
+    
+    #filtro de dados para grafico de abertura vs fechamentos
+    aberturas_por_ano = df.groupby('anoAbertura').size().reset_index(name='aberturas')
+    fechamentos_por_ano = df.dropna(subset=['anoFechamento']).groupby('anoFechamento').size().reset_index(name='fechamentos')
+
+    aberturas_por_ano.rename(columns={'anoAbertura': 'ano'}, inplace=True)
+    fechamentos_por_ano.rename(columns={'anoFechamento': 'ano'}, inplace=True)
+
+    merge_abertura_fechamento = pd.merge(aberturas_por_ano, fechamentos_por_ano, on='ano', how='outer').fillna(0)
+    
+    
+    # Total de ativas, inativas e margem_ativas_inativas
+    total_ativas = df[df['status'] == 'ativa'].shape[0]
+    total_inativas = df[df['status'] == 'inativa'].shape[0]
+    margem_ativas_inativas = total_ativas - total_inativas
+
+
+    data_margem_ativas_inativas = {
+        'Tipo': ['Ativas', 'Inativas'],
+        'Quantidade': [total_ativas, total_inativas]
+    }
+    df_margem_ativas_inativas = pd.DataFrame(data_margem_ativas_inativas)
+
+     # Filtro de dados para gráfico de ativas vs inativas
+    ativas_por_ano = df[df['status'] == 'ativa'].groupby('anoAbertura').size().reset_index(name='ativas')
+    inativas_por_ano = df[df['status'] == 'inativa'].groupby('anoFechamento').size().reset_index(name='inativas')
+
+    ativas_por_ano.rename(columns={'anoAbertura': 'ano'}, inplace=True)
+    inativas_por_ano.rename(columns={'anoFechamento': 'ano'}, inplace=True)
+
+    merge_ativas_inativas = pd.merge(ativas_por_ano, inativas_por_ano, on='ano', how='outer').fillna(0)
+    
+    
+    return (df, total_aberturas, total_fechamentos, margem_abertura_fechamento, 
+            df_margem_abertura_fechamento, merge_abertura_fechamento,
+            total_ativas, total_inativas, margem_ativas_inativas, df_margem_ativas_inativas, merge_ativas_inativas)
