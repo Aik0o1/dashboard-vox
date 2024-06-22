928ee7483c72daa34e8d429b7cfcd50191ba9682
Victor Sena <107523354+Aik0o1@users.noreply.github.com>
2024-06-22 01:06:29 -0300

    feat:Criação do módulo para gerar grafico de barras

--- graphs/graphBar.py
+++ graphs/graphBar.py
@@ -1,4 +1,3 @@
-import pandas as pd
 import plotly.express as px
 
 def plotGraphBar(df, metric, title):
@@ -7,6 +6,11 @@ def plotGraphBar(df, metric, title):
     elif metric == 'fechamentos':
         data = df.dropna(subset=['anoFechamento']).groupby('municipio').size().reset_index(name=metric)
     
+    elif metric == 'ativas':
+        data = df[df['status'] == "ativa"].groupby('municipio').size().reset_index(name=metric)
+    elif metric == 'inativas':
+        data = df[df['status'] == "inativa"].groupby('municipio').size().reset_index(name=metric)
+    
     data_sorted = data.sort_values(by=metric, ascending=False).head()
-    fig = px.bar(data_sorted, x=metric, y='municipio', orientation='h', title=f'{title} {metric.capitalize()}', height=300)
+    fig = px.bar(data_sorted, x=metric, y="municipio", orientation="h", title=f"{title.capitalize()}", height=250)
     return fig
