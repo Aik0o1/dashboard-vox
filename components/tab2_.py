6d11d088bfdbe1143694abb805d6efab14932f3f
Aik0o1 <anesrotciv@gmail.com>
2024-06-24 11:07:56 -0300

    upadate

--- components/tab2_.py
+++ components/tab2_.py
@@ -1,11 +1,11 @@
 import streamlit as st
-from graphs import mapHeat, pieMargin, lineChart, graphBar
+from graphs import mapHeat, pieMargin, graphBar, treeMap
 
 
 def layout(df, total_positivo, total_negativo, margem, tabela_margem, merge_abertura_fechamento, titulo_positivo, titulo_negativo, ano, porte, municipio, atividade):
 
     bloco_total_positivo, blocoTotalFechamentos, blocoMargem = st.columns(3)
-    bloco_grafico_abertura_fechamento = subtab1, subtab2 = st.tabs(["Gráfico", "Tabela"])
+    bloco_arvore = st.area_chart()
     bloco_mapa, bloco_grafico_barras = st.columns(2)
     blocoTabela = st.columns(1)
     
@@ -23,13 +23,7 @@ def layout(df, total_positivo, total_negativo, margem, tabela_margem, merge_aber
             st.plotly_chart(pieMargin.graph(tabela_margem), use_container_width=True)
 
 
-    with bloco_grafico_abertura_fechamento[0]:
-        subtab1.plotly_chart(lineChart.graph(merge_abertura_fechamento, titulo_positivo, titulo_negativo))
         
-    with bloco_grafico_abertura_fechamento[1]:
-        subtab2.dataframe(merge_abertura_fechamento, use_container_width=True)
-
-    
     with bloco_mapa:
         tab1, tab2 = st.tabs(["Mapa", "Tabela"])
         df_filter_map = df.copy()
@@ -47,22 +41,12 @@ def layout(df, total_positivo, total_negativo, margem, tabela_margem, merge_aber
             st.write(tabela)
 
     with bloco_grafico_barras:
-        tab1, tab2 = st.tabs(["Gráfico", "Tabela"])
-        with tab1:
-            with st.container():
-                grafico_positivo, tabela_positiva = graphBar.plotGraphBar(df, titulo_positivo, titulo_positivo)
-                grafico_negativo, tabela_negativa = graphBar.plotGraphBar(df, titulo_negativo, titulo_negativo)
+        with st.container():
+            grafico_positivo = graphBar.plotGraphBar(df, titulo_positivo, titulo_positivo)
+            grafico_negativo = graphBar.plotGraphBar(df, titulo_negativo, titulo_negativo)
 
-                st.plotly_chart(grafico_positivo, use_container_width=True)
-                st.plotly_chart(grafico_negativo, use_container_width=True)
-        with tab2:
-            col1, col2 = st.columns(2)  
-            with col1:
-                st.write(tabela_positiva)
-            with col2:
-                st.write(tabela_negativa)
-
-    with blocoTabela[0]:
-        st.title("Tabela completa")
-        st.write(df, use_container_width=True)
+            st.plotly_chart(grafico_positivo[0], use_container_width=True)
+            st.plotly_chart(grafico_negativo[0], use_container_width=True)
                     
+    with bloco_arvore:
+        st.plotly_chart(treeMap.showGraph(), use_container_width=True)
--- graphs/mapHeat.py
+++ graphs/mapHeat.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import plotly.express as px
 import geopandas as gpd
-import plotly.express as px
 
 def plotMap(df):
 
