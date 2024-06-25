8a7410c892e291f1be78eaa940b3b4cdc4be8363
Aik0o1 <anesrotciv@gmail.com>
2024-06-25 09:50:10 -0300

    Removendo cards ativo e inativo

--- components/secondTab.py
+++ components/secondTab.py
@@ -2,25 +2,14 @@ import streamlit as st
 from graphs import mapHeat, pieMargin, graphBar, treeMap
 
 
-def layout(df, total_positivo, total_negativo, margem, tabela_margem, merge_abertura_fechamento, titulo_positivo, titulo_negativo, ano, porte, municipio, atividade):
+def layout(df, ano, porte, municipio, atividade, titulo_bar1, titulo_bar2):
 
-    bloco_total_positivo, blocoTotalFechamentos, blocoMargem = st.columns(3)
     bloco_arvore = st.area_chart()
     bloco_mapa, bloco_grafico_barras = st.columns(2)
     blocoTabela = st.columns(1)
-    
-    with bloco_total_positivo:
-        st.metric(label='Total de aberturas', value=total_positivo)
 
-    with blocoTotalFechamentos:
-        st.metric(label='Total de Fechamentos', value=total_negativo)
-
-    with blocoMargem:
-        col1, col2 = st.columns(2)
-        with col1:
-            st.metric(label='Margem', value=margem)
-        with col2:
-            st.plotly_chart(pieMargin.graph(tabela_margem), use_container_width=True)
+    with bloco_arvore:
+        st.plotly_chart(treeMap.plotMapTree(df), use_container_width=True)
 
     with bloco_mapa:
         tab1, tab2 = st.tabs(["Mapa", "Tabela"])
@@ -40,11 +29,9 @@ def layout(df, total_positivo, total_negativo, margem, tabela_margem, merge_aber
 
     with bloco_grafico_barras:
         with st.container():
-            grafico_positivo = graphBar.plotGraphBar(df, titulo_positivo, titulo_positivo)
-            grafico_negativo = graphBar.plotGraphBar(df, titulo_negativo, titulo_negativo)
+            grafico_porte = graphBar.plotGraphBar(df, titulo_bar1,titulo_bar1)
+            grafico_natureza = graphBar.plotGraphBar(df, titulo_bar2, titulo_bar2)
 
-            st.plotly_chart(grafico_positivo, use_container_width=True)
-            st.plotly_chart(grafico_negativo, use_container_width=True)
+            st.plotly_chart(grafico_porte, use_container_width=True)
+            st.plotly_chart(grafico_natureza, use_container_width=True)
                     
-    with bloco_arvore:
-        st.plotly_chart(treeMap.plotMapTree(df), use_container_width=True)
--- dashboard.py
+++ dashboard.py
@@ -3,14 +3,15 @@ from components import firstTab, secondTab
 from main import load_and_prepare_data
 import pandas as pd
 
-df = pd.read_csv("assets/dadosFakess.csv")
-    
+df = pd.read_csv("assets\dadosFakes20k.csv")    
 df['abertura'] = pd.to_datetime(df['abertura'])
 df['fechamento'] = pd.to_datetime(df['fechamento'])
 df = df.sort_values(by='municipio')
+df['anoAbertura'] = df['abertura'].dt.year
+df['mesAbertura'] = df['abertura'].dt.month
+df['anoFechamento'] = df['fechamento'].dt.year
+df['mesFechamento'] = df['fechamento'].dt.month
 
-st.set_page_config(layout='wide')
-st.title("Informações Empresariais")
 
 # Criar abas
 tab1, tab2 = st.tabs(["Aberturas e Fechamentos", "Atividade e Inatividade"])
@@ -36,9 +37,8 @@ if atividade != "Todas":
 
 # Recebe dados tratados
 (total_aberturas, total_fechamentos, margem_abertura_fechamento, 
- df_margem_abertura_fechamento, merge_abertura_fechamento,
- total_ativas, total_inativas, margem_ativas_inativas, 
- df_margem_ativas_inativas, merge_ativas_inativas) = load_and_prepare_data(df_filtered)
+ df_margem_abertura_fechamento, merge_abertura_fechamento, 
+ df_porte, df_natureza) = load_and_prepare_data(df_filtered)
 
 
 #construindo a pagina
@@ -47,5 +47,5 @@ with tab1:
     firstTab.layout(df_filtered, total_aberturas, total_fechamentos, margem_abertura_fechamento, df_margem_abertura_fechamento, merge_abertura_fechamento, titulo_positivo, titulo_negativo, ano, porte, municipio, atividade)
     
 with tab2:
-    titulo_positivo, titulo_negativo = "ativas", "inativas"
-    secondTab.layout(df_filtered, total_ativas, total_inativas, margem_ativas_inativas, df_margem_ativas_inativas, merge_ativas_inativas, titulo_positivo, titulo_negativo, ano, porte, municipio, atividade)
+    titulo_bar1, titulo_bar2 = "porte", "natureza juridica"
+    secondTab.layout(df_filtered, ano, porte, municipio, atividade, titulo_bar1, titulo_bar2)
