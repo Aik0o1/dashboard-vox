5321fe181e59c8fcff0f7d590fb24bd2c03a2eb0
Cibelle Maciel <108502128+cibellemc@users.noreply.github.com>
2024-07-01 11:15:01 -0300

    feat: gráfico de porte - pie chart

--- components/firstTab.py
+++ components/firstTab.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from graphs import barChartYears, barChartMonths, mapHeat, pieMargin, graphBar, stackedBar
+from graphs import barChartYears, barChartMonths, mapHeat, pieMargin, graphBar, stackedBar, circles
 
 
 def layout(df, total_positivo, total_negativo, margem, tabela_margem, merge_abertura_fechamento, titulo_positivo, titulo_negativo, ano, porte, municipio, atividade):
@@ -7,9 +7,10 @@ def layout(df, total_positivo, total_negativo, margem, tabela_margem, merge_aber
     bloco_total_positivo, blocoTotalFechamentos, blocoMargem = st.columns(3)
     grafico_abertura_fechamento_anual = st.area_chart()
     grafico_abertura_fechamento_mensal = st.area_chart()
+    bloco_mapa, bloco_grafico_barras = st.columns(2)
     grafico_agrupado_porte, grafico_agrupado_natureza = st.columns(2)
     # grafico_arvore = st.area_chart()
-    bloco_mapa, bloco_grafico_barras = st.columns(2)
+    
     
     with bloco_total_positivo:
         st.metric(label='Total de aberturas', value=total_positivo)
@@ -24,19 +25,14 @@ def layout(df, total_positivo, total_negativo, margem, tabela_margem, merge_aber
         with col2:
             st.plotly_chart(pieMargin.graph(tabela_margem), use_container_width=True)
 
+
     with grafico_abertura_fechamento_anual:
         st.plotly_chart(barChartYears.graph(merge_abertura_fechamento))
         
     with grafico_abertura_fechamento_mensal:
         st.plotly_chart(barChartMonths.graph(df))
 
-    with grafico_agrupado_porte:
-        st.plotly_chart(stackedBar.graph(df)[0])
-        
-    
-    with grafico_agrupado_natureza:
-        st.plotly_chart(stackedBar.graph(df)[1])
-        
+
     with bloco_mapa:
         tab1, tab2 = st.tabs(["Mapa", "Tabela"])
         df_filter_map = df.copy()
@@ -60,3 +56,11 @@ def layout(df, total_positivo, total_negativo, margem, tabela_margem, merge_aber
 
             st.plotly_chart(grafico_positivo, use_container_width=True)
             st.plotly_chart(grafico_negativo, use_container_width=True)
+
+
+    with grafico_agrupado_porte:
+        st.plotly_chart(circles.graph(df), use_container_width=True)
+            
+            
+    with grafico_agrupado_natureza:
+        st.plotly_chart(stackedBar.graph(df)[1])
