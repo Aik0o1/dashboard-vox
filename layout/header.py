778e9a0f66b7d2d0d2623bc409c62c4bb4967e02
Victor Sena <107523354+Aik0o1@users.noreply.github.com>
2024-07-23 11:49:26 -0300

    feat: add head

--- layout/header.py
+++ layout/header.py
@@ -3,22 +3,19 @@ import streamlit as st
 def header():
     return (
         st.markdown("""
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
-""",unsafe_allow_html=True))
+                    <style>
+                        [data-testid="stHeader"] {
+                        display: none;
+                        }
+                        
+                        .logo {
+                            position: relative;
+                            width: 100px;
+                            left: 100px;
+                            padding: 0.5rem 0 0.5rem 0; 
+                        }
+                    </style>
+<div class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #034ea2;">
+    <img class="logo" src="https://portal.pi.gov.br/jucepi/wp-content/themes/academica_pro_3/img/logo-rodape.png"></img
+</div>
+""", unsafe_allow_html=True))
