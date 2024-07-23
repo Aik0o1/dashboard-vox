81ee1a37cff71a754db0e758f4f60a50f1a2fb2d
Cibelle Maciel <108502128+cibellemc@users.noreply.github.com>
2024-07-23 12:17:28 -0300

    feat: título e info da sidebar

---
+++ sidebar.py
@@ -0,0 +1,24 @@
+import streamlit as st
+
+def sidebar():
+    return(
+        st.markdown("""
+<style>
+    [data-testid=stSidebar] {
+        background-color: #034ea2;
+    }
+    
+    .st-emotion-cache-ue6h4q {
+                color: white
+            }
+    [data-testid="stHeading"] {
+        color: white;
+    }
+    
+    [data-testid="stNotificationContentInfo"] {
+                color: white;
+
+    }
+</style>
+""", unsafe_allow_html=True)
+    )
