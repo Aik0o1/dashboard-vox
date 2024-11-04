65bf5d737eda8b6c878a78ea6a89566a875c96da
cibellemc <cibellemaciel02@gmail.com>
2024-11-04 09:34:15 -0300

    feat: criando um usuário para acessar parte privilegiada com url

--- dashboard.py
+++ dashboard.py
@@ -48,10 +48,6 @@ font.font()
 # personalizando a sidebar
 sidebar.sidebar()
 
-
-# Cria abas
-tab1, tab2 = st.tabs(["Aberturas e Fechamentos", "Empresas Ativas"])
-
 # Sidebar
 ano = st.sidebar.selectbox("Ano", ["Todos"] + anos)
 municipio = st.sidebar.selectbox(
@@ -95,32 +91,80 @@ if atividade != "Todas":
     servico_menos_ativo,
 ) = load_and_prepare_data(df_real_filtered)
 
-# construindo a pagina
-with tab1:
-    titulo_positivo, titulo_negativo = "aberturas", "fechamentos"
-    firstTab.layout(
-        df_real_filtered,
-        total_aberturas,
-        total_fechamentos,
-        margem_abertura_fechamento,
-        df_margem_abertura_fechamento,
-        merge_abertura_fechamento,
-    )
+# Obtenha os parâmetros de consulta da URL atual
+query_params = st.query_params
 
-with tab2:
-    titulo_bar1, titulo_bar2 = "porte", "natureza juridica"
-    secondTab.layout(
-        df_real_filtered,
-        ano,
-        porte,
-        municipio,
-        atividade,
-        titulo_bar1,
-        titulo_bar2,
-        df_total_ativas,
-        servico_mais_ativo,
-        servico_menos_ativo,
-    )
+# Defina os valores salvos para usuário e senha
+usuario_salvo = "user"
+senha_salva = "pass"
+
+# Verifique se os parâmetros user e senha estão presentes na URL
+if 'user' in query_params and 'senha' in query_params:
+    # Obtenha os valores dos parâmetros
+    user_digitado = query_params['user']
+    senha_digitada = query_params['senha']
+
+    # Verifique se os valores digitados batem com os valores salvos
+    if user_digitado == usuario_salvo and senha_digitada == senha_salva:
+        # Cria abas
+        tab1, tab2 = st.tabs(["Aberturas e Fechamentos", "Empresas Ativas"])
+        # Mostrar todos os gráficos
+        with tab1:
+            titulo_positivo, titulo_negativo = "aberturas", "fechamentos"
+            firstTab.layout(
+                df_real_filtered,
+                total_aberturas,
+                total_fechamentos,
+                margem_abertura_fechamento,
+                df_margem_abertura_fechamento,
+                merge_abertura_fechamento,
+            )
+
+        with tab2:
+            titulo_bar1, titulo_bar2 = "porte", "natureza juridica"
+            secondTab.layout(
+                df_real_filtered,
+                ano,
+                porte,
+                municipio,
+                atividade,
+                titulo_bar1,
+                titulo_bar2,
+                df_total_ativas,
+                servico_mais_ativo,
+                servico_menos_ativo,
+            )
+
+    # else:
+    #     # Mostrar mensagem de erro
+    #     st.error("Usuário ou senha incorretos")
+else:
+    # Mostrar parte do conteúdo da página
+    
+            titulo_positivo, titulo_negativo = "aberturas", "fechamentos"
+            firstTab.layout(
+                df_real_filtered,
+                total_aberturas,
+                total_fechamentos,
+                margem_abertura_fechamento,
+                df_margem_abertura_fechamento,
+                merge_abertura_fechamento,
+            )
+
+    # with tab2:
+    #     titulo_bar1, titulo_bar2 = "porte", "natureza juridica"
+    #     secondTab.layout(
+    #         df_real_filtered,
+    #         ano,
+    #         porte,
+    #         municipio,
+    #         atividade,
+    #         titulo_bar1,
+    #         titulo_bar2,
+    #         df_total_ativas,
+    #         servico_mais_ativo,
+    #         servico_menos_ativo,
+    #     )
 
 # footer_html = """<hr/><div style='text-align: right; color: #d9dbdb '>
 #   <p>Developed with Python, Pandas and Streamlit</p>
